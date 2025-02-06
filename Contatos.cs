c02c0cae2525a2a9d440ea6483607aa1dff76f33
kauany Miranda <kauany.m.dantas@gmail.com>
2025-02-05 21:36:53 -0400

    Inicio

---
+++ Contatos.cs
@@ -0,0 +1,33 @@
+﻿
+using System.Numerics;
+
+public class Contatos
+{
+    private int _id;
+    public string _nome;
+    public string _phone;
+    public string _email;
+    public string _avatar;
+
+    public Contatos(string nome, string phone)
+    {
+        _phone = phone;
+        _nome = nome;
+    }
+
+    public void SetId(int id)
+    {
+        if (id >= 1)
+        {
+            _id = id;
+        }
+        else
+        {
+            throw new Exception("ID inválido");
+        }
+    }
+    public int GetId()
+    {
+        return _id;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿using System.Numerics;
+using System.Security.Cryptography;
+using static System.Console;
+using static System.Convert;
+public class Program
+{
+    
+    static void Main(string[] args)
+    {
+        List<Contatos> listContatos = new List<Contatos>();
+        do
+        {
+            Clear();
+            WriteLine("+++++ CADASTRO DE CONTATOS +++++\n");
+            WriteLine("[1] Cadastrar");
+            WriteLine("[2] Listar Contatos ");
+            WriteLine("[3] Sair ");
+            int op = ToInt32(ReadLine());
+
+            
+            if (op == 1)
+            {
+                try
+                {
+                    Write("Digite o nome: ");
+                    string nome = ReadLine();
+                    Write("Digite o phone: ");
+                    string phone = ReadLine();
+                    Contatos contato = new Contatos(nome, phone);
+                    Write("Digite o email: ");
+                    contato._email = ReadLine();
+                    Write("Digite o avatar: ");
+                    contato._avatar = ReadLine();
+                    WriteLine("\nCadastrado com sucesso!");
+                    contato.SetId(listContatos.Count + 1);
+
+                    listContatos.Add(contato);
+                    ReadKey();
+
+                }catch (Exception ex)
+                {
+                    WriteLine(ex.Message);
+                }
+            }
+            else if (op == 2)
+            {
+                Clear();
+                foreach (var agenda in listContatos)
+                {
+                    WriteLine($"\nID: {agenda.GetId}");
+                    WriteLine($"Nome: {agenda._nome}");
+                    WriteLine($"Telefone: {agenda._phone}");
+                    WriteLine($"E-mail: {agenda._email}");
+                    WriteLine($"Avatar: {agenda._avatar}\n");
+                    WriteLine("----------------------------------");
+                }
+                ReadKey();
+            }else if (op == 3)
+
+            {
+                break;
+            }
+
+
+        }
+        while (true);
+    }
+}
